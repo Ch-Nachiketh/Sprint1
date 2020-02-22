cf5c388df8ccf5abaa35956f900b77c4a7a410c8
BVRIT <BVRIT@ECE-LAB-204>
2020-02-22 10:57:58 +0530

    Test Cases

---
+++ pecuniafinance/src/test/java/com/capgemini/pecuniafinance/test/Test.java
@@ -0,0 +1,13 @@
+package com.capgemini.pecuniafinance.test;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		System.out.println("this test case is linked in testing process");
+		//fail("Not yet implemented");
+	}
+
+}
